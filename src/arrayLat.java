09d1d5bf4694c12d4d6bf73c696b259377a68680
nur.azizah <azizahn346@gmail.com>
2020-04-27 03:55:47 +0800

    Kasus lain dengan arrayLat

---
+++ src/arrayLat.java
@@ -0,0 +1,15 @@
+public class arrayLat {
+    public static void main(String[] args) {
+        String[] namaBarang = new String[5];
+
+        namaBarang[0] = "Kasur";
+        namaBarang[1] = "Bantal";
+        namaBarang[2] = "Cermin";
+        namaBarang[3] = "Sapu";
+        namaBarang[4] = "Televisi";
+
+        System.out.println(namaBarang[2]);
+    }
+
+
+}
